08934a4b07eca3269867e1a9e79c725ed781fb48
dobermann112 <ruffy112@icloud.com>
2025-09-23 22:06:44 +0900

    Gem導入時に記載を行ってなかったRSpecに関しての設定の追加

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -45,6 +45,8 @@ RSpec.configure do |config|
 
   config.include FactoryBot::Syntax::Methods
 
+  config.full_backtrace = false
+
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
 
