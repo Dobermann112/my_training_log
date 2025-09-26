bda238f1afe6a56d3de2b4290c897b624a584144
dobermann112 <ruffy112@icloud.com>
2025-09-26 21:29:41 +0900

    Userモデルにgender用のenumを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+
+  enum gender: { male: 1, female: 2 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
