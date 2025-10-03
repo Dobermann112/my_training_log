ff38cc43395d64c2a45e105493d8d67df20b5581
dobermann112 <ruffy112@icloud.com>
2025-10-03 23:15:20 +0900

    /models/workout.rb, /models/workout_set.rbにバリデーション等の追記

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,3 +1,7 @@
 class Workout < ApplicationRecord
   belongs_to :user
+  has_many :workout_sets, dependent: :destory
+
+  validates :workout_date, presence: true
+  validates :notes, length: { maximum: 500 }, allow_blank: true
 end
