2143943c3626fab3f9cb2e35495adeadb696b6d6
dobermann112 <ruffy112@icloud.com>
2025-10-03 23:39:35 +0900

    タイプミスの修正

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,6 +1,6 @@
 class Workout < ApplicationRecord
   belongs_to :user
-  has_many :workout_sets, dependent: :destory
+  has_many :workout_sets, dependent: :destroy
 
   validates :workout_date, presence: true
   validates :notes, length: { maximum: 500 }, allow_blank: true
