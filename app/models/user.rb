1fede7fb301235ae9f2e290896c9137b2fb5e3a2
dobermann112 <ruffy112@icloud.com>
2025-10-05 11:26:33 +0900

    models/user.rbにgenderの初期値をunspecifiedに設定をするよう追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,16 @@ class User < ApplicationRecord
   enum gender: { unspecified: 0, male: 1, female: 2 }
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 2 }
+
+  after_initialize :set_default_gendr, if: :new_record?
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  private
+
+  def set_default_gendr
+    self.gender ||= :unspecified
+  end
 end
