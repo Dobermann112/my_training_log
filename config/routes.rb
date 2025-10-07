6c8caef331865329cada54dac96a5b8e5834da1a
dobermann112 <ruffy112@icloud.com>
2025-10-07 22:05:58 +0900

    アカウント情報の編集画面へのルーティング定義とビューの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  resource :user, only: [:show, :edit, :update]
+  devise_scope :user do
+    get "account_edit", to: "devise/registrations#edit", as: :account_edit_user
+  end
 
+  resource :user, only: [:show, :edit, :update]
+  
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "users#show"
