6a5b10d3332e68b2d09e55977d49f501e184b24c
dobermann112 <ruffy112@icloud.com>
2025-10-08 21:44:01 +0900

    サインアウト・削除後のリダイレクト先の追記

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
 
     protected
+
+    def after_sign_out_path_for(resource_or_scope)
+      new_user_session_path
+    end
+    
   
     def configure_permitted_parameters
       added = [:name, :gender, :goal, :email, :password, :password_confirmation]
