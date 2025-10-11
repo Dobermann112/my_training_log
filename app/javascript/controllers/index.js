50f24f10575e63709d80d4adb9bfa2034adeeee6
dobermann112 <ruffy112@icloud.com>
2025-10-11 22:48:57 +0900

    esbuildのインストール

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,13 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
-import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+import { application } from "./application"
+
+// Stimulus controllerを自動登録
+const controllers = import.meta.globEager("./**/*_controller.js")
+
+for (const path in controllers) {
+  const name = path
+    .split("/")
+    .pop()
+    .replace("_controller.js", "")
+  application.register(name, controllers[path].default)
+}
