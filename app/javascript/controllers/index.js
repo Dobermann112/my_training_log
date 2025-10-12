117cfbcc75e13b27a9f1feaf974f0c885f1d0582
dobermann112 <ruffy112@icloud.com>
2025-10-13 00:14:23 +0900

    Stimulusの自動読み込みを停止

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,13 +1,5 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "./application"
 
-// Stimulus controllerを自動登録
-const controllers = import.meta.globEager("./**/*_controller.js")
-
-for (const path in controllers) {
-  const name = path
-    .split("/")
-    .pop()
-    .replace("_controller.js", "")
-  application.register(name, controllers[path].default)
-}
+// Stimulusの自動読込を停止（import.meta.globを使わない）
+export { application }
