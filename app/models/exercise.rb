6c7a3faea29e4dd2148cc33628c1b150d40be5f0
dobermann112 <ruffy112@icloud.com>
2025-10-24 20:03:24 +0900

    exerciseモデルの整備(バリデーション、スコープの追記)

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,12 +1,15 @@
 class Exercise < ApplicationRecord
     belongs_to :body_part
     belongs_to :user, optional: true
-
     has_many :workout_sets, dependent: :destroy
 
     validates :name, length: { maximum: 50 }
     validates :name, presence: true
-    validates :name, uniqueness: { scope: :user_id }
+    validates :name, uniqueness: {
+                       scope: [:user_id, :body_part_id],
+                       case_sensitive: false,
+                       message: "は同じ部位内ですでに登録されています"
+                     }
     validates :body_part_id, presence: true
     
     def default_exercise?
