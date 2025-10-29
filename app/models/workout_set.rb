7bfe03b3f52fcc5149f387f215788a9a81645d64
dobermann112 <ruffy112@icloud.com>
2025-10-29 21:00:19 +0900

    WorkoutSetモデルに自動採番メソッドを追加

--- app/models/workout_set.rb
+++ app/models/workout_set.rb
@@ -6,9 +6,17 @@ class WorkoutSet < ApplicationRecord
 
   validate :at_least_one_value_present
 
+  before_validation :set_sequential_number, on: :create
+
   private
 
   def at_least_one_value_present
     errors.add(:base, "重量、回数、メモのいずれかを入力してください") if weight.blank? && reps.blank? && memo.blank?
+  end
+
+  def set_sequential_number
+    return if set_number.present?
+    max_number = WorkoutSet.where(workout_id:, execise_id:).maximum(:set_number) || 0
+    self.set_number = max_number + 1
   end
 end
