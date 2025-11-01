e5644110cb4cb094b085df2b2e65fbb8c64a42a8
dobermann112 <ruffy112@icloud.com>
2025-11-01 10:29:21 +0900

    workout_sets_spec.rbに動作検証用のテストコードを記載

--- spec/requests/workout_sets_spec.rb
+++ spec/requests/workout_sets_spec.rb
@@ -1,39 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe "WorkoutSets", type: :request do
-  describe "GET /new" do
-    it "returns http success" do
-      get "/workout_sets/new"
-      expect(response).to have_http_status(:success)
+  let(:user) { create(:user) }
+  let(:workout) { create(:workout, user: user) }
+  let(:exercise) { create(:exercise) }
+
+  before { sign_in user }
+
+  describe "POST /workouts/:workout_id/workout_sets" do
+    it "新しいセットを作成できること" do
+      expect {
+        post workout_workout_sets_path(workout), params: {
+          workout_set: { exercise_id: exercise.id, weight: 40, reps: 10 }
+        }
+      }.to change(WorkoutSet, :count).by(1)
+      expect(response).to redirect_to(workout_path(workout))
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/workout_sets/create"
-      expect(response).to have_http_status(:success)
+  describe "PATCH /workouts/:workout_id/workout_sets/:id" do
+    let!(:workout_set) { create(:workout_set, workout: workout, exercise: exercise, weight: 50, reps: 8) }
+
+    it "セット内容を更新できること" do
+      patch workout_workout_set_path(workout, workout_set), params: {
+        workout_set: { weight: 60 }
+      }
+      expect(response).to redirect_to(workout_path(workout))
+      expect(workout_set.reload.weight).to eq(60)
     end
   end
 
-  describe "GET /edit" do
-    it "returns http success" do
-      get "/workout_sets/edit"
-      expect(response).to have_http_status(:success)
+  describe "DELETE /workouts/:workout_id/workout_sets/:id" do
+    let!(:workout_set) { create(:workout_set, workout: workout, exercise: exercise) }
+
+    it "セットを削除できること" do
+      expect {
+        delete workout_workout_set_path(workout, workout_set)
+      }.to change(WorkoutSet, :count).by(-1)
+      expect(response).to redirect_to(workout_path(workout))
     end
   end
-
-  describe "GET /update" do
-    it "returns http success" do
-      get "/workout_sets/update"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/workout_sets/destroy"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
