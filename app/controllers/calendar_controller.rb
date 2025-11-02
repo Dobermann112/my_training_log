e271a6b87e2ccc88f5d9a30a3bd1ea0a3ff7cbcf
dobermann112 <ruffy112@icloud.com>
2025-11-02 15:38:11 +0900

    calendar_controller.rbにJSONデータを返す用の記載

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,4 +1,18 @@
 class CalendarController < ApplicationController
   def index
+    @workouts = Workout.where(user: current_user)
+
+    respond_to |format|
+      format.html # index.html.erb
+      format.json do
+        render json: @workout.map { |w|
+          {
+            title: "Workout",
+            start: w.workout_date,
+            url: Rails.application.routes.url_helpers.workout_path(w)
+          }
+        }
+      end
+    end
   end
 end
