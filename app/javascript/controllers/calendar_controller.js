0ef15a372715b494541b8690e755509232486895
dobermann112 <ruffy112@icloud.com>
2025-11-02 22:17:40 +0900

    calendar_controller.jsの記載を編集

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -5,15 +5,14 @@ import dayGridPlugin from "@fullcalendar/daygrid"
 // Connects to data-controller="calendar"
 export default class extends Controller {
   connect() {
-    const calendarEl = document.getElementById("calendar")
+    const el = document.getElementById("calendar")
 
-    const calendar = new Calendar(calendarEl, {
+    const calendar = new Calendar(el, {
       plugins: [dayGridPlugin],
       initialView: "dayGridMonth",
-      locale: "ja",               // 日本語化
-      firstDay: 1,                // 月曜始まり
-      height: "auto",
-      events: "/calendars.json"   // ここでRails側からデータ取得
+      locale: "ja",
+      firstDay: 1,
+      height: "auto"
     })
 
     calendar.render()
