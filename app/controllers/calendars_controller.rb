c9d987f8fef39f28c771f6ba51afe726b55fa96a
dobermann112 <ruffy112@icloud.com>
2025-11-02 15:58:26 +0900

    Controllerへの再記載

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,4 +1,18 @@
 class CalendarsController < ApplicationController
   def index
+    @workouts = Workout.where(user: current_user)
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json do
+        render json: @workouts.map { |w|
+          {
+            title: "Workout",
+            start: w.workout_date,
+            url: Rails.application.routes.url_helpers.workout_path(w)
+          }
+        }
+      end
+    end
   end
 end
