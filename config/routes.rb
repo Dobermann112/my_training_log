ea9d7a4053b0e9922c84d06482e609f05f235f88
dobermann112 <ruffy112@icloud.com>
2025-11-03 20:04:32 +0900

    ログイン後のルートページをカレンダー画面に設定

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     authenticated :user do
-      root "users#show", as: :authenticated_root
+      root "calendars#index", as: :authenticated_root
     end
 
     unauthenticated do
