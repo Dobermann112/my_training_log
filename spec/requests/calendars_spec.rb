03e410960ce4d3f8d167bbb4924f415c19c34477
dobermann112 <ruffy112@icloud.com>
2025-11-08 16:16:12 +0900

    calendars_spec.rbにてHTTPリクエスト用のテストコードの追記

--- spec/requests/calendars_spec.rb
+++ spec/requests/calendars_spec.rb
@@ -2,10 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "Calendars", type: :request do
   describe "GET /index" do
+    let(:user) { create(:user) }
+
+    before do
+      sign_in user
+    end
+
     it "returns http success" do
-      get "/calendars/index"
+      get "/calendars"
       expect(response).to have_http_status(:success)
     end
   end
-
 end
