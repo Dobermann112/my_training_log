4c8dc665413812bdde60a484523d4f3c65e5e5f9
dobermann112 <ruffy112@icloud.com>
2025-11-08 16:01:47 +0900

    fromのタイポの修正

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -7,7 +7,7 @@ class Workout < ApplicationRecord
 
   scope :for_user, ->(user) { where(user_id: user.id) }
   scope :between, ->(from, to) {
-    return all if form.blank? && to.blank?
+    return all if from.blank? && to.blank?
     rel = all
     rel = rel.where("workout_date >= ?", from) if from.present?
     rel = rel.where("workout_date <= ?", to) if to.present?
