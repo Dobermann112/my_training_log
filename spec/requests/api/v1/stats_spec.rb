cd1a7e507884ef1e658b9f5ca1cb718d7026e771
dobermann112 <ruffy112@icloud.com>
2025-11-08 16:18:46 +0900

    RSpecにて各々のファイルに最低限の土台を記載
    - /api/v1/stats_spec.rb JSONが正しく返答されることを記載
    - /services/stats/summary_spec.rb 集計ロジックが正しく機能することを確認

---
+++ spec/requests/api/v1/stats_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe "Api::V1::Stats", type: :request do
+  let(:user) { create(:user) }
+  before { sign_in user }
+
+  describe "GET /summary" do
+    it "returns 200 with keys" do
+      get "/api/v1/stats/summary"
+      expect(response).to have_http_status(:ok)
+      json = JSON.parse(response.body)
+      expect(json.keys).to include("total_sets", "total_reps", "streak_days")
+    end
+  end
+
+  describe "GET /exercises" do
+    it "returns array" do
+      get "/api/v1/stats/exercises"
+      expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body)).to be_a(Array)
+    end
+  end
+
+  describe "GET /body_parts" do
+    it "returns array" do
+      get "/api/v1/stats/body_parts"
+      expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body)).to be_a(Array)
+    end
+  end
+end
---
+++ spec/services/stats/summary_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Stats::Summary do
+  it "calculates streak" do
+    user = create(:user)
+    # 例: 3日連続のworkoutを作る
+    3.times { |i| create(:workout, user: user, workout_date: Date.today - i) }
+    res = described_class.new(user: user).call
+    expect(res[:streak_days]).to be >= 3
+  end
+end
