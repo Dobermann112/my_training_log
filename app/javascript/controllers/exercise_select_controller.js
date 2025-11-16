3b1df83b1d5643a6f95ea8c9b262f7cdec86a45e
dobermann112 <ruffy112@icloud.com>
2025-11-16 13:30:20 +0900

    アコーディオンで選択した種目をハイライト表示

--- app/javascript/controllers/exercise_select_controller.js
+++ app/javascript/controllers/exercise_select_controller.js
@@ -4,6 +4,23 @@ export default class extends Controller {
   static targets = ["input"]
 
   select(event) {
-    console.log("clicked")
+    const clickedEl = event.target.closest(".exercise-option")
+    
+    if(!clickedEl) return
+    
+    const exerciseId = clickedEl.dataset.id
+
+    this.inputTarget.value = exerciseId
+
+    this.clearSelection()
+
+    clickedEl.classList.add("selected-exercise")
+
+    console.log("selected exercise_id:", exerciseId)
+  }
+
+  clearSelection() {
+    const options = this.element.querySelectorAll(".exercise-option")
+    options.forEach(opt => opt.classList.remove("selected-exercise"))
   }
 }
