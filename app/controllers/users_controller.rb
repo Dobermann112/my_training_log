6364ee0d2dcb7c3d4ea84576e7f1493f5b9d04cd
dobermann112 <ruffy112@icloud.com>
2025-11-21 22:34:37 +0900

    render先の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
       redirect_to dashboard_profile_path, notice: "プロフィールを更新しました"
     else
       flash.now[:alert] = "入力内容に誤りがあります"
-      render "users/account_edit", status: :unprocessable_entity
+      render "users/edit", status: :unprocessable_entity
     end
   end
 
