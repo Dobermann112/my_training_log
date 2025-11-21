c15f87a0e7f9a6c4f27c79f9489c1ee91b4d42b7
Dobermann <ruffy112@icloud.com>
2025-11-21 15:46:54 +0900

    種目追加実装（モーダル → ページ遷移方式） (#109)
    * 各部位に「＋種目を追加」ボタンを設置
    
    * トグル操作の不具合の修正
    
    * 各ビューファイルの遷移先にて[date]の保持を追加
    
    * hidden_fieldの追加
    
    * 「＋種目を追加」ボタン用のスタイルの追加
    
    * 不要スタイルの削除
    
    * 各アクションにてparams[:date]を追加し値を保持

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -4,14 +4,18 @@ class ExercisesController < ApplicationController
   before_action :authorize_exercise!, only: [:edit, :update, :destroy]
 
   def index
+    @date = params[:date]
     @exercises = Exercise.for_user(current_user).where(body_part: @body_part).includes(:body_part)
   end
 
   def new
+    @date = params[:date]
     @exercise = @body_part.exercises.build
   end
 
-  def edit; end
+  def edit
+    @date = params[:date]
+  end
 
   def create
     @exercise = @body_part.exercises.build(exercise_params)
@@ -19,24 +23,28 @@ class ExercisesController < ApplicationController
     @exercise.user = current_user if user_signed_in?
     @exercise.is_default = false if @exercise.user.present?
 
+    date = params[:date]
+
     if @exercise.save
-      redirect_to body_part_exercises_path(@body_part), notice: "種目を登録しました。"
+      redirect_to select_exercise_workouts_path(date: params[:date]), notice: "種目を登録しました。"
     else
+      @date = params[:date]
       render :new, status: :unprocessable_entity
     end
   end
 
   def update
     if @exercise.update(exercise_params)
-      redirect_to body_part_exercises_path(@body_part), notice: "種目を更新しました。"
+      redirect_to body_part_exercises_path(@body_part, date: params[:date]), notice: "種目を更新しました。"
     else
+      @date = params[:date]
       render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
     @exercise.destroy
-    redirect_to body_part_exercises_path(@body_part), notice: "種目を削除しました。"
+    redirect_to body_part_exercises_path(@body_part, date: params[:date]), notice: "種目を削除しました。"
   end
 
   private
@@ -50,8 +58,9 @@ class ExercisesController < ApplicationController
   end
 
   def authorize_exercise!
-    # 他人のカスタム・既定は編集・削除できないようにする
-    redirect_to body_part_exercise_path(@body_part), alert: "権限がありません。" unless @exercise.user.nil? || @exercise.user == current_user
+    unless @exercise.user.nil? || @exercise.user == current_user
+      redirect_to body_part_exercise_path(@body_part, date: params[:date]), alert: "権限がありません。"
+    end
   end
 
   def exercise_params
