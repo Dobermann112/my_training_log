f4146d7e86e82d65e8879ca319649e99956f0417
Dobermann <ruffy112@icloud.com>
2025-12-02 23:22:16 +0900

    React基礎学習①（useState, useEffectの使用） (#143)
    * Counter.tsxを作成しuseStateを使用するための骨組みを記載
    
    * 設定ページにてカウンターの表示
    
    - Counter.tsx useStateを使用したカウンター機能の記載
    - root.tsx Counter.tsxのimportを追加
    - settings/index.html.erb 一時的に設定ページにてReactビューの表示
    
    * Counter.tsxにて初回レンダリング時のuseEffectの使用を追加
    
    * 設定ページでのReact描画の記載を削除

---
+++ app/javascript/react/Counter.tsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+
+export default function Counter () {
+  const [count, setCount] = useState<number>(0);
+
+  useEffect(() => {
+    console.log("Counter mounted")
+  }, []);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+    </div>
+  );
+}
--- app/javascript/react/root.tsx
+++ app/javascript/react/root.tsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
+import Counter from "./Counter";
 
 function HelloReact() {
   return <div>Hello React from TSX!</div>;
@@ -11,7 +12,12 @@ document.addEventListener("turbo:load", () => {
     // 初期化済みかどうかのチェック（重複レンダリングの防止）
     if (!rootElement.hasChildNodes()) {
       const root = createRoot(rootElement);
-      root.render(<HelloReact />);
+      root.render(
+        <>
+          <HelloReact />
+          <Counter />
+        </>
+      );
     }
   }
 });
