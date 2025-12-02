594e7d3c6502960c24815f5dbf384b76f2189fb3
Dobermann <ruffy112@icloud.com>
2025-12-02 11:35:59 +0900

    最小構成でのReact基盤構築 (#142)
    * yarnコマンドでreact及びtypescriptの使用準備
    
    - yarn add react react-dom
    - yarn add -D typescript @types/react @types/react-dom
    
    * TS → JSのコンパイル用のファイルを作成
    
    * ESBuildをTSX読み込み対応の形に変更
    
    * Reactエントリポイントの作成
    
    * テスト用の最小HelloReactコンポーネントを作成

---
+++ app/javascript/react/root.tsx
@@ -0,0 +1,17 @@
+import { createRoot } from "react-dom/client";
+
+function HelloReact() {
+  return <div>Hello React from TSX!</div>;
+}
+
+document.addEventListener("turbo:load", () => {
+  const rootElement = document.getElementById("react-root");
+
+  if (rootElement) {
+    // 初期化済みかどうかのチェック（重複レンダリングの防止）
+    if (!rootElement.hasChildNodes()) {
+      const root = createRoot(rootElement);
+      root.render(<HelloReact />);
+    }
+  }
+});
