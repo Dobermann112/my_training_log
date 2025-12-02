a870110ded21a3183c45fb52e09723a673bc24ff
dobermann112 <ruffy112@icloud.com>
2025-12-02 23:04:20 +0900

    設定ページにてカウンターの表示
    - Counter.tsx useStateを使用したカウンター機能の記載
    - root.tsx Counter.tsxのimportを追加
    - settings/index.html.erb 一時的に設定ページにてReactビューの表示

--- app/javascript/react/Counter.tsx
+++ app/javascript/react/Counter.tsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
+
 export default function Counter () {
+  const [count, setCount] = useState<number>(0);
+
   return (
     <div>
-      {}
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>+1</button>
     </div>
   );
 }
