b81e75b6b1367869cf16486da8b0ef18b87f1718
dobermann112 <ruffy112@icloud.com>
2025-12-03 15:16:17 +0900

    Parent.tsxにボタンを配置し再レンダリングを実行

--- app/javascript/react/components/Parent.tsx
+++ app/javascript/react/components/Parent.tsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import Child from "./Child";
 
 export default function Parent() {
-  const [message] = useState<string>("Hello from Parent")
+  const [message, setMessage] = useState<string>("Hello from Parent")
 
   return (
     <div>
       <h3>Parent Component</h3>
       <p>Message: {message}</p>
+
+      <button onClick={() => setMessage("Updated!")}>Update Message</button>
 
       <Child message={message} />
     </div>
