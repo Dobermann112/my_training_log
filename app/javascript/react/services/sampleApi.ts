a25a83170f4a8eaabf960a6735842ba0d27761eb
dobermann112 <ruffy112@icloud.com>
2025-12-04 20:44:48 +0900

    /react/services/sampleApi.ts に1秒遅延処理を追加
    - /sampleApi.ts 内に故意に遅延処理を追加しブラウザにてLoading...が表示されることを確認
    - Turbo遷移でも正しく動くことを確認。(/setting → /calendar → /setting)

--- app/javascript/react/services/sampleApi.ts
+++ app/javascript/react/services/sampleApi.ts
@@ -1,6 +1,7 @@
 import { SampleResponse } from "react/types/api";
 
 export async function fetchSample(): Promise<SampleResponse> {
+  await new Promise((r) => setTimeout(r, 1000));
   const res = await fetch("/api/sample");
 
   if (!res.ok) {
