823e48e7f38511034335ee78176156478bb9e8c2
dobermann112 <ruffy112@icloud.com>
2025-12-08 11:06:12 +0900

    例外時にもFullCalendarが止まらないように空配列を返す記載に修正

--- app/javascript/react/services/calendarApi.ts
+++ app/javascript/react/services/calendarApi.ts
@@ -3,11 +3,17 @@ import type { CalendarEvent } from "../types/calendar";
 export async function fetchCalendarEvents(start: string,end: string): Promise<CalendarEvent[]> {
   const url = `/api/events?start=${start}&end=${end}`;
 
-  const res = await fetch(url);
+  try {
+    const res = await fetch(url);
 
-  if (!res.ok) {
-    throw new Error(`API Error: ${res.status}`);
+    if (!res.ok) {
+      console.error("API Error", res.status);
+      return [];
+    }
+
+    return res.json();
+  } catch (err) {
+    console.error("Network Error:", err);
+    return [];
   }
-
-  return res.json();
 }
