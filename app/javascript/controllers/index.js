ba460f7d2390b6331311cf9e43b3925474b7ead2
Dobermann <ruffy112@icloud.com>
2025-12-10 21:08:02 +0900

    Stimulus FullCalendar の完全削除（React 版の移行完了） (#171)
    * Calendar表示用のindex.html.erbからStimulus用の表示を削除
    
    * Stimulusコントローラの削除
    
    - calendar_controller.jsの削除
    - index.jsのCalendarControllerの登録削除

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,9 +4,6 @@
 
 import { application } from "./application"
 
-import CalendarController from "./calendar_controller"
-application.register("calendar", CalendarController)
-
 import ChartsController from "./charts_controller"
 application.register("charts", ChartsController)
 
