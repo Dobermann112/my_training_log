5452bdf3ad6f8aa7bd233583534913a64fc55084
dobermann112 <ruffy112@icloud.com>
2025-12-14 23:38:40 +0900

    UserモデルにOAuthでユーザー特定/作成するクラスメソッドの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[google_oauth2]
 
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.name = auth.info.name.presence || auth.info.email.split("@").first
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
+  
   private
 
   def set_default_gendr
