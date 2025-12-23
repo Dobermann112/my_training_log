f022e577dba4cbf23f164c5cad45d5356463d49c
dobermann112 <ruffy112@icloud.com>
2025-12-23 21:25:56 +0900

    weight と reps が両方空なら、memo があってもセットは削除する形にServiceを改装

--- app/services/workout_sets/update_one_service.rb
+++ app/services/workout_sets/update_one_service.rb
@@ -1,7 +1,6 @@
-# app/services/workout_sets/update_one_service.rb
 module WorkoutSets
   class UpdateOneService
-    Result = Struct.new(:success?, :workout_set, :errors)
+    Result = Struct.new(:success?, :workout_set, :errors, :destroyed?)
 
     def self.call(workout_set:, params:)
       new(workout_set, params).call
@@ -13,11 +12,30 @@ module WorkoutSets
     end
 
     def call
-      if @workout_set.update(@params)
-        Result.new(true, @workout_set, nil)
+      if empty_params?
+        destroy_flow
       else
-        Result.new(false, @workout_set, @workout_set.errors.full_messages)
+        update_flow
       end
+    end
+
+    private
+
+    def empty_params?
+      @params[:weight].blank? && @params[:reps].blank?
+    end
+
+    def update_flow
+      if @workout_set.update(@params)
+        Result.new(true, @workout_set, nil, false)
+      else
+        Result.new(false, @workout_set, @workout_set.errors.full_messages, false)
+      end
+    end
+
+    def destroy_flow
+      @workout_set.destroy!
+      Result.new(true, nil, nil, true)
     end
   end
 end
