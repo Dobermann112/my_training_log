587070e875770262b0c1ed2c1fb919581811afd7
dobermann112 <ruffy112@icloud.com>
2025-12-25 17:53:41 +0900

    CreateOneServiceの責務を修正
    - 未確定（draft）のセットが存在すれば、それを更新
    - 存在しなければ新しくdraft を作成
    - weight & reps 両方空なら何もしない

--- app/services/workout_sets/create_one_service.rb
+++ app/services/workout_sets/create_one_service.rb
@@ -10,14 +10,28 @@ module WorkoutSets
       @user = user
       @date = date
       @exercise_id = exercise_id
-      @params = params
+      @params = params.to_h.symbolize_keys
     end
 
     def call
       return Result.new(true, nil, nil, nil) if empty_params?
 
-      workout = find_or_create_workout
-      Result.new(true, workout, nil, nil)
+      workout = nil
+      workout_set = nil
+
+      ActiveRecord::Base.transaction do
+        workout = find_or_create_workout
+
+        workout_set =
+          find_draft_set(workout) ||
+          create_draft_set(workout)
+
+        workout_set.update!(@params)
+
+        Result.new(true, workout, workout_set, nil)
+      end
+    rescue ActiveRecord::RecordInvalid => e
+      Result.new(false, nil, nil, e.record.errors.full_messages)
     end
 
     private
@@ -28,6 +42,17 @@ module WorkoutSets
 
     def find_or_create_workout
       @user.workouts.find_or_create_by!(workout_date: @date)
+    end
+
+    def find_draft_set(workout)
+      workout.workout_sets.draft.find_by(exercise_id: @exercise_id)
+    end
+
+    def create_draft_set(workout)
+      workout.workout_sets.create!(
+        exercise_id: @exercise_id,
+        status: :draft
+      )
     end
   end
 end
