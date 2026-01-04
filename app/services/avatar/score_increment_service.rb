be2e4b25d11a5fd18a68b8df2c5c5b7a73aa22e9
dobermann112 <ruffy112@icloud.com>
2026-01-04 20:09:00 +0900

    アバタースコア加算用のServiceクラスを作成

---
+++ app/services/avatar/score_increment_service.rb
@@ -0,0 +1,15 @@
+module Avatar
+  class ScoreIncrementService
+    def initialize(workout_set)
+      @workout_set = workout_set
+    end
+
+    def call
+      # TODO: 実装は次ステップ
+    end
+
+    private
+
+    attr_reader :workout_set
+  end
+end
