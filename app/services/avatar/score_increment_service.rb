3749492d416116a1549f0bab0a5688abaddc1e5a
dobermann112 <ruffy112@icloud.com>
2026-01-04 20:58:52 +0900

    Avatarスコア用Serviceのロジックを確定
    - BodyPart → AvatarPart 変換
    - avatar_part_stats 遅延生成
    - point 加算
    - 二重加算防止
    - 編集時は無影響

--- app/services/avatar/score_increment_service.rb
+++ app/services/avatar/score_increment_service.rb
@@ -1,15 +1,51 @@
 module Avatar
   class ScoreIncrementService
+    BODY_PART_TO_AVATAR_PART = {
+      '胸' => :upper_body,
+      '背中' => :upper_body,
+      '肩' => :upper_body,
+      '腕' => :upper_body,
+      '腹' => :core,
+      '脚' => :lower_body
+    }.freeze
+
     def initialize(workout_set)
       @workout_set = workout_set
     end
 
     def call
-      # TODO: 実装は次ステップ
+      return unless should_increment?
+
+      increment_point!
     end
 
     private
 
     attr_reader :workout_set
+
+    def should_increment?
+      workout_set.created_at == workout_set.updated_at
+    end
+
+    def increment_point!
+      avatar_part_stat.increment!(:point, 1)
+      avatar_part_stat.update!(last_trained_at: Time.current)
+    end
+
+    def avatar_part
+      BODY_PART_TO_AVATAR_PART.fetch(body_part_name)
+    end
+
+    def body_part_name
+      workout_set.exercise.body_part.name
+    end
+
+    def user
+      workout_set.workout.user
+    end
+
+    def avatar_part_stat
+      user.avatar_part_stats.find_or_create_by!(avatar_part: avatar_part)
+    end
   end
 end
