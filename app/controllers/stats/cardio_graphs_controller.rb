d9f677e444b03b01ca57859d685fed01a5a4b86b
dobermann112 <ruffy112@icloud.com>
2026-01-16 12:09:03 +0900

    有酸素用のAPI Summary集計用のコントローラを作成

---
+++ app/controllers/stats/cardio_graphs_controller.rb
@@ -0,0 +1,35 @@
+class Stats::CardioGraphsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    render json: build_graph_data
+  end
+
+  private
+
+  def build_graph_data
+    range  = params[:range].presence_in(%w[week month year]) || "month"
+    period = build_period(range)
+    limit  = params[:limit].presence&.to_i || 10
+
+    {
+      period: period_info(period, range),
+      summary: summary_data(period),
+      line_duration: duration_chart(period),
+      line_distance: distance_chart(period),
+      bar_by_exercise: exercise_chart(period, limit)
+    }
+  end
+
+  def build_period(range)
+    case range
+    when "week"  then 6.days.ago.to_date..Date.current
+    when "year"  then 11.months.ago.to_date..Date.current
+    else              29.days.ago.to_date..Date.current
+    end
+  end
+
+  def period_info(period, range)
+    { from: period.first, to: period.last, range: range }
+  end
+end
