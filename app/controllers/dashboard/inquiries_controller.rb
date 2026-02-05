4e31061e1f436340633a1b7624ac56ca89571aab
dobermann112 <ruffy112@icloud.com>
2026-02-05 14:13:57 +0900

    お問い合わせフォーム用のコントローラを作成

---
+++ app/controllers/dashboard/inquiries_controller.rb
@@ -0,0 +1,28 @@
+class Dashboard::InquiriesController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @inquiry_form = InquiryForm.new(
+      name: current_user.name,
+      email: current_user.email
+    )
+  end
+
+  def create
+    @inquiry_form = InquiryForm.new(inquiry_params)
+    @inquiry_form.name ||= current_user.name
+
+    if @inquiry_form.valid?
+      # TODO: Step6で Service -> Mailer に差し替える
+      redirect_to dashboard_setting_path, notice: "フォームを送信しました"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def inquiry_params
+    params.require(:inquiry_form).permit(:name, :email, :category, :message)
+  end
+end
