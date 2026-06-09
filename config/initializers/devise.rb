a631807bccd1a7014fa5837716c9cb17b8ab8804
dobermann112 <ruffy112@icloud.com>
2026-06-09 20:02:49 +0900

    本番ビルドの設定の修正

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -29,11 +29,13 @@ Devise.setup do |config|
   config.responder.error_status = :unprocessable_entity
   config.responder.redirect_status = :see_other
 
-  config.omniauth :google_oauth2,
-    ENV["GOOGLE_CLIENT_ID"],
-    ENV["GOOGLE_CLIENT_SECRET"],
-    {
-      scope: "email,profile",
-      prompt: "select_account"
-    }
+  if ENV["GOOGLE_CLIENT_ID"].present? && ENV["GOOGLE_CLIENT_SECRET"].present?
+    config.omniauth :google_oauth2,
+      ENV["GOOGLE_CLIENT_ID"],
+      ENV["GOOGLE_CLIENT_SECRET"],
+      {
+        scope: "email,profile",
+        prompt: "select_account"
+      }
+  end
 end
